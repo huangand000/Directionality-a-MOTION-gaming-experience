231b362f593e137067b652b5db21601b98a7888b
Andre Huang <huangand000@gmail.com>
2018-03-22 19:39:20 -0700

    Opting out of angular for now

--- mddr-app/src/app/app-routing.module.ts
+++ mddr-app/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameRoomComponent } from './game-room/game-room.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: 'game-room', pathMatch: 'full'},
+  {path: 'game-room', component: GameRoomComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- mddr-app/src/app/app.module.ts
+++ mddr-app/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { HttpClientModule } from '@angular/common/http'
 import { FormsModule } from '@angular/forms'
 import { AppComponent } from './app.component';
+import { GameRoomComponent } from './game-room/game-room.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameRoomComponent
   ],
   imports: [
     BrowserModule,
