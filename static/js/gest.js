9fe0cac8e95b772c380b55f0de5d6ca2eb2c2323
Andre Huang <huangand000@gmail.com>
2018-03-23 15:24:51 -0700

    Added Timeout function, proper game loop, and basic functionality of ddr

--- static/js/gest.js
+++ static/js/gest.js
@@ -126,7 +126,8 @@ window.gest = (function (window) {
 			
 			video.width = 300;
 			video.height = 225;
-			video.setAttribute('style', '-webkit-transform: scaleX(-1); transform: scaleX(-1);'); //mirrors the video
+			// video.setAttribute('style', '-webkit-transform: scaleX(-1); transform: scaleX(-1);'); //mirrors the video
+			video.setAttribute('style', 'visibility: hidden')
 			document.body.appendChild(video);
 
 			canvas.setAttribute('style', 'width: 300px; display: none;');
