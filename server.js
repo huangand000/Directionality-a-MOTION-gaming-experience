611e9c1d79b9c371d42df3868d2896211f9b23d9
Kevin <kevinpwong@live.com>
2018-03-23 11:46:27 -0700

    Applied video to motion detection html

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ var io  = require('socket.io').listen(server);
 var path = require("path");
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-
+app.use('/static', express.static('static'));
 
 //io.set('log level', 2);
 
