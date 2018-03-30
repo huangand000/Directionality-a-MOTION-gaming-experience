a321a48ab4c37be1e03120f77af024ffffd9cb51
Andre Huang <huangand000@gmail.com>
2018-03-29 19:55:08 -0700

    Finished arrow movement on click in motion

--- static/js/game_manager.js
+++ static/js/game_manager.js
@@ -9,8 +9,15 @@ $(document).ready(function () {
     socket.on('ready', function() {
         $('#type2').html("READY!")	
     })
-
+    var song = '';
     $('#start').on('click', function() {
+        if ($('#song').val() == 'Walk it like I Talk it') {
+            $('#thisSong').attr('src', '/static/img/walk.mp3')
+            $("#audio")[0].load();
+        } else if ($('#song').val() == 'Freaky Friday') {
+            $('#thisSong').attr('src', '/static/img/music.mp3')
+            $("#audio")[0].load();
+        }
         $('#start').attr('disabled', true)
         socket.emit('gameStart')   
     })
@@ -39,7 +46,10 @@ $(document).ready(function () {
                         $('#timer').html('1');
                     }
                     if (counter < 1000) {
-                        $('#audio')[0].play();
+                
+
+                        //audio[0].play(); changed based on Sprachprofi's comment below
+                        $("#audio")[0].oncanplaythrough = $("#audio")[0].play();
                         if ($('#difficulty').val() == 'easy') {
                             spawnRate = 100;
                             speed = '-=2px'
@@ -136,98 +146,135 @@ $(document).ready(function () {
                 $('#score').html(score)
 
                 if(arrowManager.notes[0]) {
-                    if (gesture.direction == 'Left' && arrowManager.notes[0].direction == "left") {
-                        game.updateScore();
-                    } else if ((gesture.direction == 'Up' || gesture.direction == 'Long up') && arrowManager.notes[0].direction == "up") {
-                        game.updateScore();
-                    } else if ((gesture.direction == 'Down' || gesture.direction == 'Long down') && arrowManager.notes[0].direction == "down") {
-                        game.updateScore();
-                    } else if (gesture.direction == 'Right' && arrowManager.notes[0].direction == "right") {
-                        game.updateScore();
-                    } else {
-                        console.log(gesture.direction)
-                        game.hitTypes['MISS']++;
-                        $("#type").html('MISS')
-                        game.score-=10;
-                    } 
-                }
-                }, game.streak);
-                gest.start();
-            } else if ($('#mode').val() == 'normal') {
-                game.updateBoard();
-                $(document).keydown( function(event) {
-                    $('#timer').html('');
-                    $("#type").html('')
-                    $('#score').html(game.score)
-                    game.updateBoard();
-                    if(arrowManager.notes[0]) {
-                        if (event.keyCode == 37) {
-                            event.preventDefault();
-                            $('#left').attr('src', '/static/img/arrows/left2.png')
+                    if(gesture.direction == 'Left') {
+                        $('#left').attr('src', '/static/img/arrows/left2.png')
                             setTimeout(function() {
                                 $('#left').attr('src', '/static/img/arrows/staticLeft.png')
                             }, 100)
-                            if (arrowManager.notes[0].direction == "left" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 30) {
-                                $('#left').attr('src', '/static/img/arrows/left3.png')
+                        if (arrowManager.notes[0].direction == "left" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#left').attr('src', '/static/img/arrows/left3.png')
                                 setTimeout(function() {
                                     $('#left').attr('src', '/static/img/arrows/staticLeft.png')
                                 }, 200)
-                            } 
-                            game.updateScore();
-                            game.updateBoard();
-                            $('#score').html(game.score)
-                        }
-                        if (event.keyCode == 38) {
-                            event.preventDefault();
-                            $('#up').attr('src', '/static/img/arrows/up2.png')
-                            setTimeout(function() {
-                                $('#up').attr('src', '/static/img/arrows/staticUp.png')
-                            }, 100)
-                            if (arrowManager.notes[0].direction == "up" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 30) {
-                                $('#up').attr('src', '/static/img/arrows/up3.png')
-                                setTimeout(function() {
-                                    $('#up').attr('src', '/static/img/arrows/staticUp.png')
-                                }, 200)
-                            } 
-                            game.updateScore();
-                            game.updateBoard();
-                            $('#score').html(game.score)
-                        }
-                        if(event.keyCode == 40) {
-                            event.preventDefault();
-                            $('#down').attr('src', '/static/img/arrows/down2.png')
-                            setTimeout(function() {
-                                $('#down').attr('src', '/static/img/arrows/staticDown.png')
-                            }, 100)
-                            if (arrowManager.notes[0].direction == "down" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 30) {
-                                $('#down').attr('src', '/static/img/arrows/down3.png')
-                                setTimeout(function() {
-                                    $('#down').attr('src', '/static/img/arrows/staticDown.png')
-                                }, 200)
-                            } 
-                            game.updateScore();
-                            game.updateBoard();
-                            $('#score').html(game.score)
-
-                        }
-                        if(event.keyCode == 39) {
-                            event.preventDefault();
-                            $('#right').attr('src', '/static/img/arrows/right2.png')
-                            setTimeout(function() {
-                                $('#right').attr('src', '/static/img/arrows/staticRight.png')
-                            }, 100)
-                            if (arrowManager.notes[0].direction == "right" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 30) {
-                                $('#right').attr('src', '/static/img/arrows/right3.png')
-                                setTimeout(function() {
-                                    $('#right').attr('src', '/static/img/arrows/staticRight.png')
-                                }, 200)
-                            } 
-                            game.updateScore();
-                            game.updateBoard();
-                            $('#score').html(game.score)
                         }
                     }
-                });
-            }       
-        })
+                    if (gesture.direction == 'Up' || gesture.direction == 'Long up') {
+                        $('#up').attr('src', '/static/img/arrows/up2.png')
+                            setTimeout(function() {
+                                $('#up').attr('src', '/static/img/arrows/staticUp.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "up" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#up').attr('src', '/static/img/arrows/up3.png')
+                                setTimeout(function() {
+                                    $('#up').attr('src', '/static/img/arrows/staticUp.png')
+                            }, 200)
+                        } 
+                    }
+                    if (gesture.direction == 'Down' || gesture.direction == 'Long down') {
+                        $('#down').attr('src', '/static/img/arrows/down2.png')
+                            setTimeout(function() {
+                                $('#down').attr('src', '/static/img/arrows/staticDown.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "down" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#down').attr('src', '/static/img/arrows/down3.png')
+                                setTimeout(function() {
+                                    $('#down').attr('src', '/static/img/arrows/staticDown.png')
+                            }, 200)
+                        } 
+                    }
+                    if (gesture.direction == 'Right') {
+                        $('#right').attr('src', '/static/img/arrows/right2.png')
+                            setTimeout(function() {
+                                $('#right').attr('src', '/static/img/arrows/staticRight.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "right" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#right').attr('src', '/static/img/arrows/right3.png')
+                                setTimeout(function() {
+                                    $('#right').attr('src', '/static/img/arrows/staticRight.png')
+                            }, 200)
+                        }
+                    } 
+                    game.updateScore();
+                    game.updateBoard();
+                    $('#score').html(game.score)
+                }
+            }, game.streak);
+            gest.start();
+        } else if ($('#mode').val() == 'normal') {
+            game.updateBoard();
+            $(document).keydown( function(event) {
+                $('#timer').html('');
+                $("#type").html('')
+                $('#score').html(game.score)
+                game.updateBoard();
+                if(arrowManager.notes[0]) {
+                    if (event.keyCode == 37) {
+                        event.preventDefault();
+                        $('#left').attr('src', '/static/img/arrows/left2.png')
+                        setTimeout(function() {
+                            $('#left').attr('src', '/static/img/arrows/staticLeft.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "left" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#left').attr('src', '/static/img/arrows/left3.png')
+                            setTimeout(function() {
+                                $('#left').attr('src', '/static/img/arrows/staticLeft.png')
+                            }, 200)
+                        } 
+                        game.updateScore();
+                        game.updateBoard();
+                        $('#score').html(game.score)
+                    }
+                    if (event.keyCode == 38) {
+                        event.preventDefault();
+                        $('#up').attr('src', '/static/img/arrows/up2.png')
+                        setTimeout(function() {
+                            $('#up').attr('src', '/static/img/arrows/staticUp.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "up" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#up').attr('src', '/static/img/arrows/up3.png')
+                            setTimeout(function() {
+                                $('#up').attr('src', '/static/img/arrows/staticUp.png')
+                            }, 200)
+                        } 
+                        game.updateScore();
+                        game.updateBoard();
+                        $('#score').html(game.score)
+                    }
+                    if(event.keyCode == 40) {
+                        event.preventDefault();
+                        $('#down').attr('src', '/static/img/arrows/down2.png')
+                        setTimeout(function() {
+                            $('#down').attr('src', '/static/img/arrows/staticDown.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "down" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#down').attr('src', '/static/img/arrows/down3.png')
+                            setTimeout(function() {
+                                $('#down').attr('src', '/static/img/arrows/staticDown.png')
+                            }, 200)
+                        } 
+                        game.updateScore();
+                        game.updateBoard();
+                        $('#score').html(game.score)
+
+                    }
+                    if(event.keyCode == 39) {
+                        event.preventDefault();
+                        $('#right').attr('src', '/static/img/arrows/right2.png')
+                        setTimeout(function() {
+                            $('#right').attr('src', '/static/img/arrows/staticRight.png')
+                        }, 100)
+                        if (arrowManager.notes[0].direction == "right" && arrowManager.notes[0].image.position().top > 0 && arrowManager.notes[0].image.position().top < 45) {
+                            $('#right').attr('src', '/static/img/arrows/right3.png')
+                            setTimeout(function() {
+                                $('#right').attr('src', '/static/img/arrows/staticRight.png')
+                            }, 200)
+                        } 
+                        game.updateScore();
+                        game.updateBoard();
+                        $('#score').html(game.score)
+                    }
+                }
+            });
+        }       
+    })
 });
