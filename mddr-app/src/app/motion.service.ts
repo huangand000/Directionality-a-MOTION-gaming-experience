7f089a77130518310c439a4b8e308d8bbed4772d
Andre Huang <huangand000@gmail.com>
2018-03-30 10:33:21 -0700

    added ejs

--- mddr-app/src/app/motion.service.ts
+++
@@ -1,9 +0,0 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http' 
-
-@Injectable()
-export class MotionService {
-
-  constructor(private _http: HttpClient) { }
-
-}
